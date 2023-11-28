1b8cc433b8ec1b167b2bbb3c934105e53c9bf41e
Nicholas Steinbauer <web@obrien.com>
2023-11-28 16:29:39 -0500

    few changes and favicon

--- src/components/global/Scroller.client.jsx
+++ src/components/global/Scroller.client.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-scroll';
 export default function Scroller() {
   return (
     <div className="scroller text-center">
-      <Link to="subHero" spy={true} smooth={true} duration={1000}>
+      <Link to="new2024">
         <div>
           <Image src="/scroller.gif" alt="Scroll Down" width={36} height={36} />
         </div>
