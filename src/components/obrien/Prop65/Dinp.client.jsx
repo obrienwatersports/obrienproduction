3c4fc32673295f041c34e8de7cae74c8b1eb2cb6
obrienwatersports <nsteinbauer@obrien.com>
2024-01-18 13:51:51 -0500

    oopsie

--- src/components/obrien/Prop65/Dinp.client.jsx
+++ src/components/obrien/Prop65/Dinp.client.jsx
@@ -3,27 +3,23 @@ import './Prop65.scss';
 function Dinp() {
   return (
     <>
-      <h3>Prop 65 Warning: Di-n-hexyl Phthalate (DnHP)</h3>
+      <h3>Prop 65 Warning: Diisononyl Phthalate (DINP)</h3>
       <div className="field__item">
         <div className="paragraph paragraph--type--warning paragraph--view-mode--default factsheet-item theme--warning">
           <div className="factsheet-item__title field__item">
-            Why am I being warned about potential exposure to DnHP?
+            Why am I being warned about potential exposure to DINP?
           </div>
           <div className="factsheet-item__content field__item">
             <ul>
               <li>
-                DnHP is on the&nbsp;
+                DINP is on the&nbsp;
                 <a href="https://www.p65warnings.ca.gov/faq">Proposition 65</a>
-                &nbsp;list because it can cause birth defects or other
-                reproductive harm.&nbsp;
+                &nbsp;list because it can cause cancer.
               </li>
-              <li>
-                Exposure to DnHP may harm the male and female reproductive
-                systems.&nbsp;
-              </li>
+              <li>Exposure to DINP may increase the risk of cancer.</li>
               <li>
                 Proposition 65 requires businesses to determine if they must
-                provide a warning about exposure to&nbsp;
+                provide a warning about exposures to&nbsp;
                 <a href="https://www.p65warnings.ca.gov/chemicals">
                   listed chemicals
                 </a>
--- src/components/obrien/Prop65/Dnhp.client.jsx
+++ src/components/obrien/Prop65/Dnhp.client.jsx
@@ -3,23 +3,27 @@ import './Prop65.scss';
 function Dnhp() {
   return (
     <>
-      <h3>Prop 65 Warning: Diisononyl Phthalate (DINP)</h3>
+      <h3>Prop 65 Warning: Di-n-hexyl Phthalate (DnHP)</h3>
       <div className="field__item">
         <div className="paragraph paragraph--type--warning paragraph--view-mode--default factsheet-item theme--warning">
           <div className="factsheet-item__title field__item">
-            Why am I being warned about potential exposure to DINP?
+            Why am I being warned about potential exposure to DnHP?
           </div>
           <div className="factsheet-item__content field__item">
             <ul>
               <li>
-                DINP is on the&nbsp;
+                DnHP is on the&nbsp;
                 <a href="https://www.p65warnings.ca.gov/faq">Proposition 65</a>
-                &nbsp;list because it can cause cancer.
+                &nbsp;list because it can cause birth defects or other
+                reproductive harm.&nbsp;
               </li>
-              <li>Exposure to DINP may increase the risk of cancer.</li>
+              <li>
+                Exposure to DnHP may harm the male and female reproductive
+                systems.&nbsp;
+              </li>
               <li>
                 Proposition 65 requires businesses to determine if they must
-                provide a warning about exposures to&nbsp;
+                provide a warning about exposure to&nbsp;
                 <a href="https://www.p65warnings.ca.gov/chemicals">
                   listed chemicals
                 </a>
